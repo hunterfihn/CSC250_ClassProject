82f29320c41398ce412773ce292f6ff8d460368a
hunterfihn <101016842+hunterfihn@users.noreply.github.com>
2022-03-28 12:02:56 -0500

    Init Commit

---
+++ ClassProject1/Assets/Scripts/boxScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxScript : MonoBehaviour
+{
+    public Vector3 goUp, goForward, goBack, goLeft, goRight;
+    public float speed = 2.0f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start() // Like a constructor
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            rb.velocity = Vector3.up * speed;
+
+        }
+        else if (Input.GetKeyDown("w"))
+        {
+            rb.velocity = Vector3.forward * speed;
+
+        }
+        else if (Input.GetKeyDown("s"))
+        {
+            rb.velocity = Vector3.back * speed;
+
+        }
+        else if (Input.GetKeyDown("a"))
+        {
+            rb.velocity = Vector3.left * speed;
+        }
+        else if (Input.GetKeyDown("d"))
+        {
+            rb.velocity = Vector3.right * speed;
+        }
+
+    }
+}
