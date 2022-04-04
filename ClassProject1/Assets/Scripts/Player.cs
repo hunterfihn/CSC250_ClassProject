a07d532e756718c7f7f2abc85dc5087b1703d56d
hunterfihn <101016842+hunterfihn@users.noreply.github.com>
2022-04-04 12:16:09 -0500

    Rebuild Room and doors

---
+++ ClassProject1/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    string name;
+
+    public Player(string name)
+    {
+        this.name = name;
+    }
+
+    public string getName()
+    {
+        return this.name;
+    }
+}
---
+++ ClassProject1/Assets/Scripts/playerScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    private Player thePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePlayer = new Player("Hunter");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(thePlayer.getName());
+    }
+}
